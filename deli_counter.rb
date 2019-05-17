c2954f2b4bad88fc0c113beca71be3919b112ece
Trish Ogen <patriciaogen@gmail.com>
2019-05-17 01:31:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ end
 
 def now_serving(array)
   if array.length == 0
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array[0]}"
   end
