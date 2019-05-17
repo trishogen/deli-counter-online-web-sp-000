a1ce1f5818fa7c1f0dcea12411d04558c2eb0cc4
Trish Ogen <patriciaogen@gmail.com>
2019-05-17 01:21:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 def line(queue)
+  phrase = "The line is currently:"
   if queue.length == 0
     puts "The line is currently empty."
   else
     queue.each_with_index do |person, index|
-      phrase = "The line is currently:" << " #{index + 1}. #{person}"
+      phrase << " #{index + 1}. #{person}"
     end
     puts phrase
   end
